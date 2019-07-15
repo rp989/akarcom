cdd4941962c0c15808c334c16e8ca0368a5d2fe7
rp996 <riad.sasila@gmail.com>
2019-07-15 06:01:11 -0700

    test upload

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+define('CMS_URI', 'http://Your-site.loc');
+
+// ID_KEY onesignal.com
+define('APPID', 'cf67acea-dde2-4034-9e4a-0edd37827645');
+define('APKEY', 'OGM2YjgwN2QtMTIyYy00ZTBjLWEzODItNjI4MDViNTQwZTZh');
+// Start massege
+define('ACTIONMESSAGE', 'application');
+define('ACCEPTBUTTONTEXT', 'ALLOW');
+define('CANCELBUTTONTEXT', 'NO THANKS');
+define('URLDEFOLTIMG', 'http://ci3onesignal.codefort.ru/push/img/9397-ya.timkas-priroda-azar-013.gif');
+
+
+?>
